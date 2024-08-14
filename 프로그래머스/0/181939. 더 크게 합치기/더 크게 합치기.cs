48e1c3402cd99350360ba20f5b1e32a7abfc9241
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-14 18:56:03 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.26 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cs
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cs
@@ -3,12 +3,15 @@ using System;
 public class Solution {
     public int solution(int a, int b) {
         int answer = 0;
+        // 1.
         string a1 = a.ToString();
         string b1 = b.ToString();
         
+        // 2.
         int answer1 = int.Parse(a1+b1);
         int answer2 = int.Parse(b1+a1);
         
+        // 3.
         if(answer1 >= answer2) {
             answer = answer1;
         }
