4bb9a9063c05c27f0a600104fc9d9d863d1d7aad
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-19 01:13:11 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 44.89 ms, Memory: 48.6 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.cs
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.cs
@@ -4,11 +4,13 @@ public class Solution
 {
     public int solution(int[] arr) 
     {
-        bool check = false;
-        int count = 0;
+        bool check = false; // 조건 체크용 bool변수
+        int count = 0; // 반복 횟수
         
+        // 무한루프로 배열을 조건에 맞게 계속 변환하는 기능
         while(true) {
-            
+            // 매 반복마다 배열의 idx를 하나씩 하나씩 점검하며 아래의 조건에 맞는지 찾아준다. 
+            // 만약 맞다면 그 idx에 들어있는 값을 조건에 맞게 변환후 check를 true로 변환
             for(int i=0; i<arr.Length; i++) {
                 if(arr[i] >= 50 && arr[i]%2 == 0) {
                     arr[i] /= 2;
@@ -19,10 +21,12 @@ public class Solution
                     arr[i]++;
                     check = true;
                 }
-            }   
+            }
+            // 만약 check값이 false라면 while 반복문을 탈출해준다.
             if (!check) {
                 break;
             }
+            // 만약 check 값이 true라면 check 값을 false로 바꾸어주고 반복횟수를 하나 올려준다.
             check = false;
             count ++;
         }
