90d54f1ac977af28f6a4e43965bbab9510c93996
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-20 11:44:37 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.85 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution 
+{
+    public int[] solution(int[] arr) 
+    {
+        List<int> arrList = new List<int>();
+        for(int i=0; i<arr.Length; i++) {
+            for(int j=0; j<arr[i]; j++) {
+                arrList.Add(arr[i]);
+            }
+        }
+        return arrList.ToArray();
+    }
+}
