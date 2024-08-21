18ded83cb1c15262a3826a96aab03c96156d6698
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-21 21:14:38 +0900

    [level 0] Title: 문자열 묶기, Time: 1.33 ms, Memory: 49.2 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.cs
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
         
@@ -7,40 +6,10 @@ public class Solution
 {
     public int solution(string[] strArr) 
     {
-        // strArr의 각각의 요소에 담긴 문자열의 길이를 요소로할 리스트를 선언
-        List<int> numList = new List<int>();
-        // 리스트에 strArr[i]의 길이를 하나씩 하나씩 할당
+        int[] answer = new int[31];
         for(int i=0; i<strArr.Length; i++) {
-            numList.Add(strArr[i].ToString().Length);
+            answer[strArr[i].Length]++;
         }
-        numList.Sort(); // 리스트 정렬
-        
-        List<int> countList = new List<int>();
-        int count = 0;
-        int previousNum = -1;
-        foreach (int num in numList) 
-        {
-            if (num == previousNum) {
-                count++;
-            } 
-            else {
-                if (previousNum != -1) {
-                    countList.Add(count);
-                }
-                previousNum = num;
-                count = 1;
-            }
-        }
-        
-        // 마지막 빈도수 추가
-        if (previousNum != -1) 
-        {
-             countList.Add(count);
-        }
-        
-        countList.Sort();
-
-        // 결과를 배열로 변환
-        return countList[countList.Count - 1];
+        return answer.Max();
     }     
 }
