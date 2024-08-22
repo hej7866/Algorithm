25dc3f57faf61ffc5fc1d1eb2f5385b69d6398c5
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-23 02:49:18 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.86 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+public class Solution 
+{
+    public int[] solution(int[] array) 
+    {
+        int max = array.Max();
+        int[] result = new int[2];
+        result[0] = max;
+        for(int i=0; i<array.Length; i++) {
+            if(array[i] == max) {
+                result[1] = i;
+            }
+        }
+        return result;
+    }
+}
