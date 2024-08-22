f8e6a1164a5e47a5eaa245cff3b76d1fb643f187
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-23 00:47:49 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.30 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution 
+{
+    public int solution(string str1, string str2) 
+    {
+        int answer = 0;
+        return answer = str1.Contains(str2) ? 1 : 2;
+    }
+}
