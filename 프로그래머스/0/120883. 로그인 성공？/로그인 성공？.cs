0ec6c9a71b80390c80f75ebb63d77dc2cbde7247
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-24 17:57:17 +0900

    [level 0] Title: 로그인 성공?, Time: 0.45 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Solution 
+{
+    public string solution(string[] id_pw, string[,] db) 
+    {
+        bool idpw = false;
+        string result = "";
+        
+        int row = db.GetLength(0);
+        for(int r=0; r<row; r++) {
+            if(id_pw[0] == db[r,0] && id_pw[1] == db[r,1]) {
+                idpw = true;
+                result = "login";
+            }
+        }
+        
+        bool id = false;
+        bool pw = false;
+        if(!idpw) {
+            for(int r=0; r<row; r++) {
+                if(id_pw[0] == db[r,0]) {
+                    id = true;
+                    result = "wrong pw";
+                }
+                else if(!id && id_pw[1] == db[r,1]) {
+                    pw = true;
+                    result = "fail";
+                }
+            }
+        }
+        return result;
+    }
+}
