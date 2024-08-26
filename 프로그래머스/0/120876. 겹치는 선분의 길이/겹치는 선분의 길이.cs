7fdebd41f97e0ea2336ae75398e5f91ad2aa49cb
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-26 14:51:05 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.42 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[,] lines) 
+    {
+        int answer = 0;
+        int max = 0;
+        int min = 0;
+        int[] lineArr = new int[200];
+        
+        int row = lines.GetLength(0);
+        for(int i = 0; i < row; i++)
+        {
+            max = Math.Max(lines[i,0], lines[i,1]);
+            min = Math.Min(lines[i,0], lines[i,1]);
+            for(int j = min; j < max; j++)
+            {
+                lineArr[j + 100]++;
+            }
+        }
+        for(int i = 0; i < lineArr.Length; i++)
+        {
+            if(lineArr[i] > 1)
+            {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
