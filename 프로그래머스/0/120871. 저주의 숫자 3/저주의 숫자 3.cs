5f9a10597ed78086398ceac4b5b28c964cbaa5f1
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-28 05:43:53 +0900

    [level 0] Title: 저주의 숫자 3, Time: 1.71 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution 
+{
+    public int solution(int n) 
+    {
+        List<int> threeX = new List<int>();
+        
+        int index = 1;
+        threeX.Add(index);
+        while(index <= 300) 
+        {
+            if(threeX[threeX.Count - 1]%3 != 0 && !threeX[threeX.Count - 1].ToString().Contains("3")) 
+            {    
+                threeX.Add(index);
+                index++;
+            }
+            if(threeX[threeX.Count - 1]%3 == 0 || threeX[threeX.Count - 1].ToString().Contains("3")) 
+            {    
+                threeX[threeX.Count - 1]++;
+                index++;
+            }
+        }
+        threeX.RemoveAt(0);
+        return threeX[n-1];
+        
+    }
+}
