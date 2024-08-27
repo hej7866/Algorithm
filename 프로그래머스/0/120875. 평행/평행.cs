7014f58a9ebd6c6e6c81d2b5dfbb561dafc1d008
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-28 00:12:35 +0900

    [level 0] Title: 평행, Time: 0.62 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.cs
+++ 프로그래머스/0/120875. 평행/평행.cs
@@ -1,77 +1,89 @@
 using System;
-using System.Collections.Generic;
 
-public class CartesianCoordinate
-{
-    public int X = 0;
-    public int Y = 0;
-
-    public bool IsSame(CartesianCoordinate other)
+public class Solution {
+    public bool IsParallel(int x1, int y1, int x2, int y2)
     {
-        return X == other.X && Y == other.Y;
+        return y1 * x2 == y2 * x1;
+    }
+
+    public int solution(int[,] dots) {
+        bool parallel =
+IsParallel(dots[0, 0] - dots[1, 0], dots[0, 1] - dots[1, 1], dots[2, 0] - dots[3, 0], dots[2, 1] - dots[3, 1]) 
+||
+IsParallel(dots[0, 0] - dots[2, 0], dots[0, 1] - dots[2, 1], dots[1, 0] - dots[3, 0], dots[1, 1] - dots[3, 1]) 
+||
+IsParallel(dots[0, 0] - dots[3, 0], dots[0, 1] - dots[3, 1], dots[1, 0] - dots[2, 0], dots[1, 1] - dots[2, 1]);
+
+        return parallel ? 1 : 0;
     }
 }
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
 
-public class LineInformation
-{
-    public CartesianCoordinate Point1 = new CartesianCoordinate();
-    public CartesianCoordinate Point2 = new CartesianCoordinate();
-
-    public double Slope
-    {
-        get
-        {
-            return (double)(Point2.Y - Point1.Y) / (Point2.X - Point1.X);
-        }
-    }
-
-    public LineInformation(int x1, int y1, int x2, int y2)
-    {
-        Point1.X = x1;
-        Point1.Y = y1;
-        Point2.X = x2;
-        Point2.Y = y2;
-    }
-
-    public bool IsPointShared(LineInformation other)
-    {
-        return Point1.IsSame(other.Point1) || Point1.IsSame(other.Point2) ||
-            Point2.IsSame(other.Point1) || Point2.IsSame(other.Point2);
-    }
-
-    public bool IsParallel(LineInformation other)
-    {
-        return Slope == other.Slope;
-    }
-}
-
-public class Solution
-{
-    public int solution(int[,] dots)
-    {
-        List<LineInformation> lines = new List<LineInformation>();
-
-        for (int i = 0; i < dots.GetLength(0); i++)
-        {
-            for (int j = i + 1; j < dots.GetLength(0); j++)
-            {
-                var lineInfo = new LineInformation(dots[i, 0], dots[i, 1], dots[j, 0], dots[j, 1]);
-
-                lines.Add(lineInfo);
-            }
-        }
-
-        for (int i = 0; i < lines.Count; i++)
-        {
-            for (int j = i + 1; j < lines.Count; j++)
-            {
-                if (!lines[i].IsPointShared(lines[j]) && lines[i].IsParallel(lines[j]))
-                {
-                    return 1;
-                }
-            }
-        }
-
-        return 0;
-    }
-}
+// public class Solution 
+// {
+//     public int solution(int[,] dots) 
+//     {
+//         int row = dots.GetLength(0); // dots의 행의 갯수 (여기선 4개로 고정)
+//         int[,] lines = new int[6,2]; // dots가 4개이므로 선은 6개가 생길것임.
+        
+//         int index = 0;
+//         for(int r=0; r<row; r++) {
+//             for(int j=r+1; j<row; j++) { // 종점 - 시점을 해주면 선이 하나 생성이 되는것을 이용하여 선 6개를 만들어줌
+//                 lines[index,0] = dots[j,0] - dots[r,0]; 
+//                 lines[index,1] = dots[j,1] - dots[r,1];
+//                 index++;
+//             }
+//         }
+//         List<float> slopeList = new List<float>();
+//         for(int i=0; i<lines.GetLength(0); i++) {
+//             int dx = lines[i,0];
+//             int dy = lines[i,1];
+//             slopeList.Add((float)dy/dx);
+//         }
+        
+//        var duplicates = slopeList.GroupBy(n => n)         // 원소를 그룹화
+//                                  .Where(g => g.Count() > 1) // 원소가 2번 이상 나타나는 그룹 필터링
+//                                  .Select(g => g.Key)      // 중복된 원소 선택
+//                                  .ToArray();
+//                 for(int i=0; i<lines.GetLength(0); i++) {
+//             int dx = lines[i,0];
+//             int dy = lines[i,1];
+            
+//             bool isNegativeX = false;
+//             bool isNegativeY = false;
+//             if(dx < 0) {
+//                 dx = -dx;
+//                 isNegativeX = true;
+//             }
+//             else if(dy < 0) {
+//                 dy = -dy;
+//                 isNegativeY = true;
+//             }
+            
+//             int j = 2;
+//             while(j<=dx) {
+//                 while(dx%j == 0 && dy%j == 0) {
+//                     dx = dx/j;
+//                     dy = dy/j;
+//                 }
+//                 j++;
+//             }
+//             lines[i,0] = (isNegativeX) ? -dx : dx;
+//             lines[i,1] = (isNegativeY) ? -dy : dy;
+//         }
+        
+//         int count = 0;
+//         for(int i=0; i<lines.GetLength(0); i++) {
+//             for(int j=i+1; j<lines.GetLength(0); j++) {
+//                 if(lines[i,0] == lines[j,0] && lines[i,1] == lines[j,1]) {
+//                     count++;
+//                 }
+//             }
+//         }
+        
+//         return duplicates.Length > 0 ? 1 : 0;
+//     }
+// }
+        
