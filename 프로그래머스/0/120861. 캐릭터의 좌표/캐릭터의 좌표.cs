1727cd5716a999b3bbad47314751dfdbf5bc013c
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-29 01:17:51 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.34 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cs
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cs
@@ -4,39 +4,39 @@ public class Solution
 {
     public int[] solution(string[] keyinput, int[] board) 
     {
-        int[] playerPos = {0,0};
+        int[] playerPos = {0,0}; // 플레이어 좌표의 초기값 입력
         
-        int boundaryLeft  = -((board[0] - 1) / 2);
-        int boundaryRight =  ((board[0] - 1) / 2);
-        int boundaryUp    =  ((board[1] - 1) / 2);
-        int boundaryDown  = -((board[1] - 1) / 2);
+        int boundaryLeft  = -((board[0] - 1) / 2); // board의 경계선 할당 (왼쪽)
+        int boundaryRight =  ((board[0] - 1) / 2); // board의 경계선 할당 (오른쪽)
+        int boundaryUp    =  ((board[1] - 1) / 2); // board의 경계선 할당 (위쪽)
+        int boundaryDown  = -((board[1] - 1) / 2); // board의 경계선 할당 (아래쪽)
         for(int i=0; i<keyinput.Length; i++)
         {
             switch (keyinput[i])
             {
                 case "left":
-                    if(playerPos[0] == boundaryLeft) 
+                    if(playerPos[0] == boundaryLeft)  // 만약 현재 위치가 왼쪽 경계선과 같다면 반복문으로돌아가라
                     {
                         continue;
                     }
                     playerPos[0]--;
                     break;
                 case "right":
-                    if(playerPos[0] == boundaryRight) 
+                    if(playerPos[0] == boundaryRight)  // 만약 현재 위치가 오른쪽 경계선과 같다면 반복문으로돌아가라
                     {
                         continue;
                     }
                     playerPos[0]++;
                     break;
                 case "up":
-                    if(playerPos[1] == boundaryUp) 
+                    if(playerPos[1] == boundaryUp)  // 만약 현재 위치가 위쪽 경계선과 같다면 반복문으로돌아가라
                     {
                         continue;
                     }
                     playerPos[1]++;
                     break;
                 case "down":
-                    if(playerPos[1] == boundaryDown) 
+                    if(playerPos[1] == boundaryDown)  // 만약 현재 위치가 아래쪽 경계선과 같다면 반복문으로돌아가라
                     {
                         continue;
                     }
