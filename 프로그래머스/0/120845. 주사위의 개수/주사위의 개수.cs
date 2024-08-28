8044dcc46e7662e9ddaf7d318d364fab32df8b51
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-29 03:20:42 +0900

    [level 0] Title: 주사위의 개수, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int[] box, int n) 
+    {
+        int x = box[0] / n;
+        int y = box[1] / n;
+        int z = box[2] / n;
+        
+        return x * y * z;
+    }
+}
