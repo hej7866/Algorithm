e2c48dbebd1f5abee0eee9d201ec9c2240f789ab
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-08-30 00:32:17 +0900

    [level 0] Title: 개미 군단, Time: 0.18 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int hp) 
+    {
+        int count = 0;
+        while(hp >= 5)
+        {
+            hp -= 5;
+            count++;
+        }
+        while(hp >= 3)
+        {
+            hp -= 3;
+            count++;
+        }
+        while(hp != 0)
+        {
+            hp -= 1;
+            count++;
+        }
+        return count;
+    }
+}
