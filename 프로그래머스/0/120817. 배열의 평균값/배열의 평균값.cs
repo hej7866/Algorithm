167913d7bb79938f86ffcaa65250647023cbf00e
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-01 00:55:25 +0900

    [level 0] Title: 배열의 평균값, Time: 0.77 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq; 
+
+public class Solution 
+{
+    public double solution(int[] numbers) 
+    {
+        double average = numbers.Average();
+        return average;
+    }
+}
