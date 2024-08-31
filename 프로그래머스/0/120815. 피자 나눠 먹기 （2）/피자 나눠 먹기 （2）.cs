e97d92681d47074062f844ff6910bfc7dcd409b9
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-01 01:10:11 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.20 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n) 
+    {
+        int count = 1;
+        while((6*count) % n != 0)
+        {
+            count++;
+        }
+        return count;
+    }
+}
