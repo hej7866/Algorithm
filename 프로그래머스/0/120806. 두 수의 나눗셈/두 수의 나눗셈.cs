3379256d2146387d79b6833070a7dcc4d79d5a13
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-02 01:36:00 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.14 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int num1, int num2) 
+    {
+        double result = ((double)num1/num2) * 1000;
+        return (int)result;
+    }
+}
