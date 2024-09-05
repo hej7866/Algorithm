5f51f3ee0d468dc7c65d541ee8f0004a280d6316
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-06 00:31:28 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/README.md
+++ 프로그래머스/0/181934. 조건 문자열/README.md
@@ -1,10 +1,10 @@
 # [level 0] 조건 문자열 - 181934 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181934) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181934?language=cpp) 
 
 ### 성능 요약
 
-메모리: 31.4 MB, 시간: 0.37 ms
+메모리: 4.21 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 10일 03:51:08
+2024년 09월 06일 00:31:26
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) 
+{
+    int answer = 0;
+    if(ineq == "<")
+    {
+        if(eq == "=")
+        {
+            answer = (n <= m) ? 1 : 0;
+        }
+        else if(eq == "!")
+        {
+            answer = (n < m) ? 1 : 0;
+        }
+    }
+    else if(ineq == ">")
+    {
+        if(eq == "=")
+        {
+            answer = (n >= m) ? 1 : 0;
+        }
+        else if(eq == "!")
+        {
+            answer = (n > m) ? 1 : 0;
+        }
+    }
+    return answer;
+}
