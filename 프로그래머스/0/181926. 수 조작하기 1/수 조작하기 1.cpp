9ab94c2a53c58144f00b1d95906bd4544113ff7b
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-06 01:56:26 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.78 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cpp
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -25,3 +25,19 @@ int solution(int n, string control)
     }
     return n;
 }
+
+
+
+// #include <string>
+// #include <vector>
+// #include <map>
+// using namespace std;
+
+// map <char, int> m = {{'w', 1}, {'s', -1}, {'d', 10}, {'a', -10}};
+// int solution(int n, string control) 
+// {
+//     int answer = n;
+//     for(char ch : control)
+//         answer += m[ch];
+//     return answer;
+// }
