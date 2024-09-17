5fcc5a5206be64747e5fe4e6bfea87c95e8c99fb
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-17 23:43:01 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) 
+{
+    if(price >= 100000 && price < 300000) { return price*0.95; }
+    else if(price >= 300000 && price < 500000) { return price*0.9; }
+    else if(price >= 500000) { return price*0.8; }
+    else { return price; }
+}
