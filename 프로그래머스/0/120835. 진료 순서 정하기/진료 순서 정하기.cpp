40b6000e6d4841ee6719bdb4d9bdca98a8322633
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-17 21:22:34 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
@@ -19,3 +19,18 @@ vector<int> solution(vector<int> emergency)
     }
     return answer;
 }
+
+
+// #include <string>
+// #include <vector>
+// #include <map>
+// using namespace std;
+
+// vector<int> solution(vector<int> emergency) {
+//     auto amt = map<int, int>();
+//     for (auto& e : emergency) { amt[e]; }
+//     auto order = amt.size();
+//     for (auto& e : amt) { e.second = order--; }
+//     for (auto& e : emergency) { e = amt[e]; }
+//     return emergency;
+// }
