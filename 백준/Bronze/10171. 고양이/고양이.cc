a88cae40ee4da6e7f875d0250921767092d437ca
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-22 03:37:17 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+/*
+* 고양이를 출력한다.
+*/
+int main()
+{
+    cout << "\\    /\\" << "\n" << " )  ( ')" << "\n" << "(  /  )" << "\n" << " \\(__)|";
+    return 0;
+}
+
