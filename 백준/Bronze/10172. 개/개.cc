9f60766a5b2155113f6bda6e24926c6e2721b4ad
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-22 03:40:43 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+/*
+* 고양이를 출력한다.
+*/
+int main()
+{
+    cout << "|\\_/|" << "\n" << "|q p|   /}" << "\n" << "( 0 )\"\"\"\\" << "\n" << "|\"^\"`    |" << "\n" << "||_/=\\\\__|" ;
+    return 0;
+}
+
