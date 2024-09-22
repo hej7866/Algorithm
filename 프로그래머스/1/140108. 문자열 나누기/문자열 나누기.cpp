f15444cbcf55faf09137dfe77cdd97c0c802e5f9
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-23 03:06:30 +0900

    [level 1] Title: 문자열 나누기, Time: 0.37 ms, Memory: 3.77 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -6,46 +6,56 @@ using namespace std;
 
 int solution(string s) 
 {
+    // 이 문제의 경우 굳이 vec를 만들필요는 없다
+    // 하지만 분해된 문자열을 담은 벡터를 반환하라는 문제가 나올수도 있으니 다음과 같이 해결하였다.
+    
+    // 분해된 문자열을 담을 벡터
     vector<string> vec;
     
+    // 처음 idx를 0으로 초기화
     int idx = 0;
-    while(true)
+    
+    // 조건을 만족하기 전까진 루프를 반복할 것.
+    while(true) 
     {
-        char ch = s[idx];
+        char x = s[idx]; // x에 s의 idx번째 값을 담는다. 
         
-        int eNum = 0;
-        int dNum = 0;
-        string tmp = "";
+        int eNum = 0; // x와 같은 글자의 수를 담을 변수
+        int dNum = 0; // x와 다른 글자의 수를 담을 변수
+        string tmp = ""; // 분해된 문자열을 담을 변수
+        
+        // eNum과 dNum이 같아져서 멈춘것인지 아니면 s의 모든 인덱스를 순환해서 멈춘것인지 확인하는 변수
         bool isBool = false;
-        for(int i=idx; i<s.length(); i++)
+        for(int i=idx; i<s.length(); i++) // idx부터 s.length()까지 순환
         {
-            if(ch == s[i]) { eNum++; }
-            else if(ch != s[i]) { dNum++; }
+            if(x == s[i]) { eNum++; } // s[i]가 x와 같다면 eNum++;
+            else if(x != s[i]) { dNum++; } // s[i]가 x와 다르다면 dNum++;
             
-            tmp += s[i];
-            if(eNum == dNum)
+            tmp += s[i]; // tmp엔 s[i]를 붙혀넣음 (자른 문자열이 되는 것)
+            if(eNum == dNum) // 만약 eNum 와 dNum이 같다면 
             {
-                vec.push_back(tmp);
-                tmp = "";
-                if(i+1 < s.length()) { idx = i+1; }
-                else if(i+1 == s.length()) { break; }
-                isBool = true;
+                vec.push_back(tmp); // tmp,즉 자른 문자열을 vec에 넣고
+                tmp = ""; // tmp를 빈 문자열로 초기화
+                if(i+1 < s.length()) { idx = i+1; } // 만약 i+1이 s.length()보다 작다면 idx 를 i+1로 초기화
+                else if(i+1 == s.length()) { break; } // i+1이 s.length()과 같다면 마지막 글자인것이므로 그냥 break
+                isBool = true; // eNum과 dNum이 같아서 멈춘것이므로 true
                 break;
             }
         }
-        if(!isBool)
+        if(!isBool) // eNum과 dNum이 같아서 멈춘것이 아니라면
         {
-            if(!tmp.empty())
+            if(!tmp.empty()) // tmp가 비어있지 않으면
             {
-                vec.push_back(tmp);
+                vec.push_back(tmp); // vec에 값을 추가
             }
-            break;
+            break; // 이때는 마지막 글자인것이므로 while 반복문을 탈출.
         }
     }
-    for(string str : vec)
-    {
-        cout << str << " ";
-    }
+    // 디버그용
+        // for(string str : vec)
+        // {
+        //     cout << str << " ";
+        // }
     return vec.size();
 }
 
