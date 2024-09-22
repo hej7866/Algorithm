7c09843db9b87109031586d83dd4a1f05d6e8192
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-23 02:52:57 +0900

    [level 1] Title: 문자열 나누기, Time: 0.59 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -48,3 +48,36 @@ int solution(string s)
     }
     return vec.size();
 }
+
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ참고한 코드ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ //
+
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+// int solution(string s) {
+//     int answer = 0;
+//     int xCount = 1;
+//     int yCount = 0;
+//     char x = s[0];
+
+//     for (int i = 1; i < s.size(); i++) {
+//         if (s[i] == x) {
+//             xCount++;
+//         } else {
+//             yCount++;
+//         }
+
+//         if (xCount == yCount) {
+//             xCount = 0;
+//             yCount = 0;
+//             answer++;
+//             x = s[i + 1];
+//         }
+//     }
+
+//     if (xCount > 0) answer++;
+
+//     return answer;
+// }
