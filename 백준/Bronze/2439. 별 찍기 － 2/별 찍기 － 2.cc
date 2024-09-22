1f57db037d22e87182069073d837ee8650ab0906
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-23 08:32:05 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+ 
+/* 문제 
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+*/
+
+
+/* 입력
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+*/
+
+
+/* 출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+*/
+
+
+/* 제한
+
+*/
+
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    string star = "";
+
+    for(int i=0; i<N; i++)
+    {
+        star += " ";
+    }
+
+    for(int i=N-1; i>=0; i--)
+    {
+        star[i] = '*';
+        cout << star << "\n";
+    }
+    return 0;
+}
+
