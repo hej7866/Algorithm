3e8f30b58b84575afc308ccb07a05cfea66b69de
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-24 08:01:06 +0900

    [level 1] Title: 기사단원의 무기, Time: 59.69 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -8,6 +8,9 @@ int solution(int number, int limit, int power)
     int sum = 0;
     
     // 시간 복잡도 해결해야하는 문제.
+    // 처음엔 단순하게 약수구하는 방법으로 계산을 했더니(O(n^2)) 실행시간 초과
+    
+    // 시간 복잡도를 최적화하며 약수 구하는 방법으로 다시 계산을 했더니 통과.
     for(int i=1; i<=number; i++)
     {
         int count = 0;
