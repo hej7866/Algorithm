4da0958003ac8060fea8eb9dbdfd179a62a33bf9
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-23 09:07:52 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+ 
+/* 문제 
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+*/
+
+
+/* 입력
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+*/
+
+
+/* 출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+*/
+
+
+/* 제한
+
+*/
+
+
+int main()
+{
+    int a, b;
+    while(cin >> a >> b)
+    {
+        cout << a + b << "\n";
+    }
+    return 0;
+}
+
