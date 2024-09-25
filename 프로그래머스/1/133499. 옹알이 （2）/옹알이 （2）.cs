966120f9cd38e2fc744ad1517755074c0d57bd2c
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-25 20:54:22 +0900

    [level 1] Title: 옹알이 (2), Time: 4.89 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cs
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cs
@@ -5,17 +5,20 @@ public class Solution
 {
     public int solution(string[] babbling) 
     {
-        Dictionary<string, string> dic = new Dictionary<string, string>();
+        // 옹알이에 해당하는 문자열을 key로 그거에 대응하는 value를 특수문자로 저장하기 위한 Dictionary
+        Dictionary<string, string> dic = new Dictionary<string, string>(); 
         
-        string[] Key = new string[] {"aya", "ye", "woo", "ma"};
-        string[] Value = new string[] {"@", "#", "$", "%"};
+        string[] Key = new string[] {"aya", "ye", "woo", "ma"}; // key가 될 배열
+        string[] Value = new string[] {"@", "#", "$", "%"};     // value가 될 배열
         
         int answer = 0;
+        // dic에 key와 value를 할당하는 과정
         for(int i=0; i<Key.Length; i++)
         {
             dic[Key[i]] = Value[i];
         }
         
+        // babbling에 들어있는 각 요소를 변형하는 과정
         for(int i=0; i<babbling.Length; i++)
         {
             for(int j=0; j<Key.Length; j++)
@@ -27,37 +30,40 @@ public class Solution
             }
         }
         
+        // 그렇게 만들어진 각 요소들이 네 가지 발음을 이어붙힌 형태로만 이루어져있으며
+        // 같은 발음이 연속되지는 않는지 점검하는 로직
         foreach(string str in babbling)
         {
-            bool isBool = false;
+            bool isBool = false; // babbling의 요소를 이루는 각 글자가 value의 원소들 중 하나인지 체크하는 변수
             for(int i=0; i<str.Length; i++)
             {
                 if(str[i] == '@' || str[i] == '#' || str[i] == '$' || str[i] == '%')
                 {
-                    isBool = true;
+                    isBool = true; // str[i]가 @ # $ % 중 하나라면 true
                 }
                 else
                 {
-                    isBool = false;
-                    break;
+                    isBool = false; // 그렇지 않으면 false로 바꾸고
+                    break;          // 반복문 탈출
                 }
             }
             
-            bool isSame = false;
-            if(isBool)
+            bool isSame = false; // 발음이 연속되어 있는지 체크하는 변수
+            if(isBool) // 만약 babbling의 요소를 이루는 각 글자가 모두 value의 원소들 중 하나라면
             {
                 for(int i=0; i<str.Length - 1; i++)
                 {
-                    if(str[i] == str[i+1])
+                    if(str[i] == str[i+1]) // str의 i번째 글자와 i+1번째 글자가 같다면
                     {
-                        isSame = true;
-                        break;
+                        isSame = true; // 발음이 연속되는 것이다.
+                        break;         // 발음이 연속된다면 break;
                     }
                 }
             }
-            if(isBool && !isSame)
+            // babbling의 요소를 이루는 각 글자가 모두 value의 원소들 중 하나이고 발음이 연속되지않는다면
+            if(isBool && !isSame) 
             {
-                answer++;
+                answer++; // answer 1증가
             }
             Console.WriteLine(answer);
         }
