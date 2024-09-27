dbfadfd04d892bfe4859a11f2b7f7c3311fd681b
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-28 05:20:26 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+ 
+/* 문제 
+알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+*/
+
+
+/* 입력
+알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+*/
+
+
+/* 출력
+입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+*/
+
+
+/* 제한
+
+*/
+
+
+int main()
+{
+    char c;
+    cin >> c;
+
+    cout << (int)c;
+
+    return 0;
+}
+
