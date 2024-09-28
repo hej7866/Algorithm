d5e39184155307018de09b1242de81c312da5941
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-09-29 07:36:24 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+ 
+/* 문제 
+
+*/
+
+
+/* 입력
+
+*/
+
+
+/* 출력
+
+*/
+
+
+/* 제한
+
+*/
+
+
+int main()
+{
+    cout << "         ,r\'\"7" << "\n" << "r`-_   ,'  ,/" << "\n" << " \\. \". L_r'" << "\n" << "   `~\\/" << "\n" << "      |" << "\n" << "      |";
+
+    return 0;
+}
+
