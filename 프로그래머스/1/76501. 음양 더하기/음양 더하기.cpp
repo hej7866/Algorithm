27a27cc07d915d1c3e001a694bf9ad1610c05bd6
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-10-02 19:31:13 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) 
+{
+    int sum = 0;
+    for(int i=0; i<signs.size(); i++)
+    {
+        if(signs[i])
+        {
+            sum += absolutes[i];
+        }
+        else if(!signs[i])
+        {
+            sum -= absolutes[i];
+        }
+    }
+    return sum;
+}
