e797f87bffda8e42a9f9c7e47c282d6b41eb88bd
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-10-06 03:11:31 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.14 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) 
+{
+    int len = board.size();
+    vector<stack<int>> stacks(len);
+
+    // 배열의 요소를 각 스택에 분배
+    for (int i = len-1; i >= 0; i--) 
+    {
+        for (int j = 0; j < len; ++j) 
+        {
+            stacks[j].push(board[i][j]); // j번째 스택에 i번째 행의 요소 추가
+        }
+    }
+    
+    for (int i = 0; i < len; ++i) 
+    {
+        while(stacks[i].top() == 0) 
+        {
+            stacks[i].pop();
+        } 
+    }
+    
+    int count = 0;
+    vector<int> vec;
+    for(int &i : moves)
+    {
+        if(!stacks[i-1].empty())
+        {
+            if(!vec.empty() && vec.back() == stacks[i-1].top())
+            {
+                vec.pop_back();
+                count++;
+            }
+            else if(vec.empty() || vec.back() != stacks[i-1].top())
+            {     
+                vec.push_back(stacks[i-1].top());
+            }
+            stacks[i-1].pop();
+        }
+    }
+    
+    for (int i = 0; i < len; ++i) 
+    {
+        cout << (i + 1) << "번 스택: ";
+        while (!stacks[i].empty()) 
+        {
+            cout << stacks[i].top() << " ";
+            stacks[i].pop();
+        }
+        cout << endl;
+    }
+    
+    for(int v : vec)
+    {
+        cout << v << " ";
+    }
+    
+    return count  * 2;
+}
