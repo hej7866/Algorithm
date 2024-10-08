befb207bb3119317958df55c71c5c24fc7bd9add
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-10-09 05:42:56 +0900

    [level 1] Title: 행렬의 덧셈, Time: 56.39 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) 
+{
+    vector<vector<int>> answer;
+    
+    int row = arr1.size();
+    int col = arr1[0].size();
+    
+    for(int i=0; i<row; i++)
+    {
+        vector<int> vec;
+        for(int j=0; j<col; j++)
+        {
+            vec.push_back(arr1[i][j] + arr2[i][j]);
+        }
+        answer.push_back(vec);
+    }
+    
+    return answer;
+}
