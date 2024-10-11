9649f163d22441a7de36bb4e2b321db5a319ed40
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-10-12 00:45:47 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    string s = to_string(n);
+    int sum = 0;
+    for(char &c : s)
+    {
+        sum += c - '0';
+    }
+    return sum;
+}
