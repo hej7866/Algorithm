fa5e1ed282e83ef4a2a46e33abb29f56740f8a90
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-10-15 01:46:32 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.11 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) 
+{
+    string ans = "";
+    for(int i=0; i<n; i++)
+    {
+        if(i%2 == 0)
+        {
+            ans += "수";
+        }
+        else
+        {
+            ans += "박";
+        }
+    }
+    return ans;
+}
