5cb441f5932a747231863bea02efa2ed70cd3b7b
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-10-16 17:04:07 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,53 +1,44 @@
 #include <iostream>
-#include <string>
-#include <sstream>
 #include <vector>
-#include <algorithm>
-#include <map>
-#include <set>
 
 using namespace std;
 
+void insertionSort(vector<int> vec, int n)
+{
+    int j;
+    for (int i = 0; i < n - 1; i++)
+    {
+        j = i;
+        while (vec[j] > vec[j + 1] && j >= 0)
+        {
+            swap(vec[j], vec[j + 1]);
+            j--;
+        }
+    }
 
-/* 문제 
-N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
-*/
-
-
-/* 입력
-첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다. 
-*/
-
-
-/* 출력
-첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
-*/
-
-
-/* 제한
-
-*/
-
-
+    for (int i = 0; i < n; i++)
+    {
+        cout << vec[i] << "\n";
+    }
+}
 
 int main() 
 {
+    ios::sync_with_stdio(false);  // 입출력 속도 최적화
+    cin.tie(NULL);
+
+    
     int N;
     cin >> N;
-
-    set<int> set;
-    int a;
+    vector<int> v;
     for(int i=0; i<N; i++)
     {
+        int a;
         cin >> a;
-        set.insert(a);
+        v.push_back(a);
     }
+    
+    insertionSort(v, v.size());
 
-    for(auto &i : set)
-    {
-        cout << i << "\n";
-    }
-
-    return 0;           
+    return 0;
 }
-
