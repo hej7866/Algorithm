dae2600225e6c90879f213b4623d365903867a3d
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-10-22 16:44:39 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+
+
+/* 문제 [재귀]
+피보나치 수는 0과 1로 시작한다. 0번째 피보나치 수는 0이고, 1번째 피보나치 수는 1이다. 그 다음 2번째 부터는 바로 앞 두 피보나치 수의 합이 된다.
+이를 식으로 써보면 Fn = Fn-1 + Fn-2 (n ≥ 2)가 된다.
+n=17일때 까지 피보나치 수를 써보면 다음과 같다.
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
+n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
+
+
+/* 입력
+첫째 줄에 n이 주어진다. n은 45보다 작거나 같은 자연수이다.
+
+/* 출력
+첫째 줄에 n번째 피보나치 수를 출력한다.
+
+/* 제한
+*/
+
+int FibDp(int n)
+{
+	int* dp = new int[n + 1];
+
+	dp[0] = 0;
+	dp[1] = 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+
+	return dp[n];
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    cout << FibDp(N);
+    return 0;
+}
