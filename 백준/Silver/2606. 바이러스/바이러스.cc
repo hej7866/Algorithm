f370680fcf8ed56f8b2c6e4a672ce982ecf577b4
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-10-29 17:27:23 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+
+/* 문제 [그래프 이론 / 그래프 탐색 / DFS / BFS]
+신종 바이러스인 웜 바이러스는 네트워크를 통해 전파된다. 
+한 컴퓨터가 웜 바이러스에 걸리면 그 컴퓨터와 네트워크 상에서 연결되어 있는 모든 컴퓨터는 웜 바이러스에 걸리게 된다.
+예를 들어 7대의 컴퓨터가 <그림 1>과 같이 네트워크 상에서 연결되어 있다고 하자. 
+1번 컴퓨터가 웜 바이러스에 걸리면 웜 바이러스는 2번과 5번 컴퓨터를 거쳐 3번과 6번 컴퓨터까지 전파되어 2, 3, 5, 6 네 대의 컴퓨터는 웜 바이러스에 걸리게 된다. 
+하지만 4번과 7번 컴퓨터는 1번 컴퓨터와 네트워크상에서 연결되어 있지 않기 때문에 영향을 받지 않는다.
+어느 날 1번 컴퓨터가 웜 바이러스에 걸렸다. 
+컴퓨터의 수와 네트워크 상에서 서로 연결되어 있는 정보가 주어질 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 출력하는 프로그램을 작성하시오.
+
+/* 입력
+첫째 줄에는 컴퓨터의 수가 주어진다. 컴퓨터의 수는 100 이하인 양의 정수이고 각 컴퓨터에는 1번 부터 차례대로 번호가 매겨진다. 
+둘째 줄에는 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수가 주어진다. 
+이어서 그 수만큼 한 줄에 한 쌍씩 네트워크 상에서 직접 연결되어 있는 컴퓨터의 번호 쌍이 주어진다.
+
+/* 출력
+1번 컴퓨터가 웜 바이러스에 걸렸을 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 첫째 줄에 출력한다.
+
+/* 제한
+*/
+
+using namespace std;
+
+// 백준문제 - 바이러스 [2606]
+
+vector<vector<int>> graph(101);
+bool visited[101] = { false };
+
+int n, m; // n 컴퓨터 수, m(엣지)의 수
+int cnt = 0; // 1번 컴퓨터와 연결된 컴퓨터의 수
+
+void DFS(int node) // 현재 연결된 컴퓨텨 (node)
+{
+    for (int i = 0; i < graph[node].size(); i++)
+    {
+        int currentNode = graph[node][i];
+
+        if (visited[currentNode]) continue;
+        visited[currentNode] = true;
+        cnt++;
+        DFS(currentNode);
+    }
+}
+
+
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b; // 컴퓨터, 정점
+
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    // 컴퓨터 1 감염
+    visited[1] = true;
+    DFS(1);
+
+    cout << cnt << "\n";
+    return 0;
+}
