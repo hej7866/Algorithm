70b023da69f1b5d9d9e980b75941c18266aa0875
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-11-05 02:20:28 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+
+// 백준문제 - 가장 긴 증가하는 부분 수열 [11053]
+
+/* 문제 [DP]
+수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
+예를 들어, 수열 A = {10, 20, 10, 30, 20, 50} 인 경우에 가장 긴 증가하는 부분 수열은 A = {10, 20, 10, 30, 20, 50} 이고, 길이는 4이다.
+첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 100보다 작거나 같은 자연수이다.
+
+/* 입력
+첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다.
+둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
+
+/* 출력
+첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
+
+/* 제한
+*/
+
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() 
+{
+    int N;
+    cin >> N;
+
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    // DP 배열 생성: 각 위치에서 끝나는 가장 긴 증가하는 부분 수열의 길이를 저장
+    vector<int> dp(N, 1);
+
+    int maxLength = 1;
+
+    // DP 배열 채우기
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            if (A[i] > A[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        maxLength = max(maxLength, dp[i]);
+    }
+
+    cout << maxLength << endl;
+
+    return 0;
+}
