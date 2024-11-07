09de0cb62a4b9e9cb5a0ede1ce7f06b9036ffc10
hej7866 <157799662+hej7866@users.noreply.github.com>
2024-11-07 18:55:34 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+
+// 백준문제 - 전깃줄 [2565]
+
+/* 문제 [DP]
+두 전봇대 A와 B 사이에 하나 둘씩 전깃줄을 추가하다 보니 전깃줄이 서로 교차하는 경우가 발생하였다. 
+합선의 위험이 있어 이들 중 몇 개의 전깃줄을 없애 전깃줄이 교차하지 않도록 만들려고 한다.
+
+예를 들어, < 그림 1 >과 같이 전깃줄이 연결되어 있는 경우 A의 1번 위치와 B의 8번 위치를 잇는 전깃줄, 
+A의 3번 위치와 B의 9번 위치를 잇는 전깃줄, A의 4번 위치와 B의 1번 위치를 잇는 전깃줄을 없애면 남아있는 모든 전깃줄이 서로 교차하지 않게 된다.
+
+
+전깃줄이 전봇대에 연결되는 위치는 전봇대 위에서부터 차례대로 번호가 매겨진다. 
+전깃줄의 개수와 전깃줄들이 두 전봇대에 연결되는 위치의 번호가 주어질 때, 남아있는 모든 전깃줄이 서로 교차하지 않게 하기 위해 없애야 하는 전깃줄의 최소 개수를 구하는 프로그램을 작성하시오.
+
+/* 입력
+첫째 줄에는 두 전봇대 사이의 전깃줄의 개수가 주어진다. 전깃줄의 개수는 100 이하의 자연수이다. 
+둘째 줄부터 한 줄에 하나씩 전깃줄이 A전봇대와 연결되는 위치의 번호와 B전봇대와 연결되는 위치의 번호가 차례로 주어진다. 
+위치의 번호는 500 이하의 자연수이고, 같은 위치에 두 개 이상의 전깃줄이 연결될 수 없다.
+
+/* 출력
+첫째 줄에 남아있는 모든 전깃줄이 서로 교차하지 않게 하기 위해 없애야 하는 전깃줄의 최소 개수를 출력한다.
+
+/* 제한
+*/
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<pair<int, int>> wires(N);
+    for(int i=0; i<N; i++){
+        cin >> wires[i].first >> wires[i].second;
+    }
+    
+    // 왼쪽 전봇대 기준으로 정렬
+    sort(wires.begin(), wires.end());
+    
+    // 오른쪽 전봇대의 위치를 추출
+    vector<int> B;
+    for(int i=0; i<N; i++) B.push_back(wires[i].second);
+    
+    // LIS를 위한 DP 배열
+    vector<int> dp;
+    for(auto b : B)
+    {
+        // b가 dp에 존재하는지 확인
+        auto it = lower_bound(dp.begin(), dp.end(), b);
+        if(it == dp.end()) dp.push_back(b);
+        else *it = b;
+    }
+    
+    // 최소 제거 개수 = 전체 - LIS 길이
+    cout << N - dp.size();
+    return 0;
+}
