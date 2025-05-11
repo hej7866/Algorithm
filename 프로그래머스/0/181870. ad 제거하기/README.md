243bd4d0153111fe9e10871504f8e1f2ca4ea527
hej7866 <157799662+hej7866@users.noreply.github.com>
2025-05-11 20:49:28 +0900

    [level 0] Title: ad 제거하기, Time: 0.54 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/README.md
+++ 프로그래머스/0/181870. ad 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] ad 제거하기 - 181870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181870?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181870) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.46 ms
+메모리: 4.21 MB, 시간: 0.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 07일 18:23:26
+2025년 05월 11일 20:49:26
 
 ### 문제 설명
 
--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cpp
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cpp
@@ -1,24 +1,18 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 vector<string> solution(vector<string> strArr) 
 {
-    vector<string> answer;
-    string ad = "ad";
-    for(int i=0; i<strArr.size(); i++)
+    string target = "ad";
+    vector<string> ans;
+    for(string s : strArr)
     {
-        // 특정 위치에 특정문자열이 있는지 점검하는방법은 compare로 찾는다.
-        // 이건 전체 문자열중에 특정문자열이 있는지만 점검하면 되기때문에 search메서드를 쓴다.
-        // 매개변수 1,2 =>첫 번째 범위의 시작과 끝을 나타내는 반복자입니다. 검색을 수행할 범위를 지정.
-        // 매개변수 3,4 =>두 번째 범위의 시작과 끝을 나타내는 반복자입니다. 찾으려는 서브시퀀스(부분 문자열)를 지정.
-        if(search(strArr[i].begin(), strArr[i].end(), ad.begin(), ad.end()) != strArr[i].end())
+        if(s.find(target) == string::npos)
         {
-            continue;
+            ans.push_back(s);
         }
-        answer.push_back(strArr[i]);
     }
-    return answer;
+    return ans;
 }
