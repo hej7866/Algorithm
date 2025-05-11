addb8245141fdb3a7843f4284fb7b9ed52b111d2
hej7866 <157799662+hej7866@users.noreply.github.com>
2025-05-11 20:59:31 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/README.md
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열이 몇 번 등장하는지 세기 - 181871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181871?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181871) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.03 ms
+메모리: 4.14 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 07일 18:11:50
+2025년 05월 11일 20:59:31
 
 ### 문제 설명
 
--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.cpp
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.cpp
@@ -6,12 +6,11 @@ using namespace std;
 int solution(string myString, string pat) 
 {
     int count = 0;
-    for(int i=0; i<=myString.size() - pat.size(); i++)
+    size_t pos = myString.find(pat);
+    while (pos != string::npos) 
     {
-        if(myString.compare(i,pat.size(), pat) == 0) // compare(시작위치, 검사할 길이, 검사할 문자열) 있다면 0 반환 
-        {
-            count++;
-        }    
+        count++;
+        pos = myString.find(pat, pos + 1); // 겹쳐서 나오는 것도 찾으려면 +1로 이동
     }
     return count;
 }
