a6e4fd9378361e00055efab8f6fdaa5c145660b5
Eun Joong Hwang <157799662+hej7866@users.noreply.github.com>
2025-10-24 21:05:14 +0900

    [level 0] Title: a와 b 출력하기, Time: 3.16 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -6,7 +6,7 @@ int main(void) {
     int a;
     int b;
     cin >> a >> b;
-    cout << "a" << " = " << a << endl;
-    cout << "b" << " = " << b << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
     return 0;
 }
