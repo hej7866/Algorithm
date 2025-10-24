cf26609381a590f719beb16dd0880e1e1ce99ece
Eun Joong Hwang <157799662+hej7866@users.noreply.github.com>
2025-10-24 22:59:32 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 39.79 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/README.md
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 조건에 맞게 수열 변환하기 2 - 181881 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181881?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181881) 
 
 ### 성능 요약
 
-메모리: 32.7 MB, 시간: 30.45 ms
+메모리: 36.4 MB, 시간: 39.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 07일 07:37:12
+2025년 10월 24일 22:59:31
 
 ### 문제 설명
 
--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.cpp
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.cpp
@@ -1,33 +1,29 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> arr) 
 {
     int count = 0;
-    while(true)
-    {      
-        bool isCheck = false;
-        for(int i=0; i<arr.size(); i++)
-        {
-            if(arr[i] >= 50 && arr[i]%2 == 0)
-            {
-                arr[i] /= 2;
-                isCheck = true;
-            }
-            else if(arr[i] < 50 && arr[i]%2 == 1)
-            {
-                arr[i] *= 2;
-                arr[i] += 1;
-                isCheck = true;
-            }
-        }
-        if(!isCheck)
-        {
-            break;
-        }
+    vector<int> tmp;
+    while(tmp != arr)
+    {
         count++;
+        tmp = arr;
+        for(int &n : arr)
+        {
+            if(n >= 50 && n%2 == 0)
+            {
+                n /= 2;
+            }
+            else if (n < 50 && n%2 == 1)
+            {
+                n *= 2;
+                n += 1;
+            }
+        }
     }
-    return count;
+    return count - 1;
 }
