3025ec6fa59cc3e56d153e23c0e9e14dc6e9a352
Eun Joong Hwang <157799662+hej7866@users.noreply.github.com>
2025-10-29 22:40:02 +0900

    [level 0] Title: 배열 조각하기, Time: 1.59 ms, Memory: 6.71 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cpp
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cpp
@@ -1,21 +1,21 @@
 #include <string>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> arr, vector<int> query) 
 {
-    // begin() => 0번째 인덱스를 가르킴
-    // end() => 마지막 인덱스 + 1를 가르킴 [,) 
     for(int i=0; i<query.size(); i++)
     {
         if(i%2 == 0)
         {
-            arr.erase(arr.begin() + query[i] + 1, arr.end()); // query[i] + 1번째 인덱스부터 마지막 인덱스까지 삭제
+            arr.erase(arr.begin() + query[i] + 1, arr.end());
         }
         else
         {
-            arr.erase(arr.begin(), arr.begin() + query[i]); // 0번째 인덱스부터 query[i]번째 인덱스 전까지 삭제
+            arr.erase(arr.begin(), arr.begin() + query[i]);
         }
     }
     return arr;
