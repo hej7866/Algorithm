064e27b88f2c0f2522196e2602e565fa3b3e5a04
Eun Joong Hwang <157799662+hej7866@users.noreply.github.com>
2025-10-30 20:59:50 +0900

    [level 0] Title: 세로 읽기, Time: 0.09 ms, Memory: 3.75 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.cpp
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.cpp
@@ -5,18 +5,25 @@ using namespace std;
 
 string solution(string my_string, int m, int c) 
 {
-    vector<string> strVec;
+    vector<string> v;
+    string s = "";
+    int n = -1;
     
-    for(int i=0; i<my_string.length(); i = i + m)
+    for(int i=0; i<my_string.size(); i++)
     {
-        strVec.push_back(my_string.substr(i,m));   
+        n = i + 1;
+        s.push_back(my_string[i]);
+        if(n%m == 0)
+        {
+            v.push_back(s);
+            s = "";
+        }
     }
     
-    string result = "";
-    for(int i=0; i<strVec.size(); i++)
+    string ans;
+    for(string s : v)
     {
-        string temp = strVec[i];
-        result += temp[c-1];
+        ans += s[c-1];
     }
-    return result;
+    return ans;
 }
