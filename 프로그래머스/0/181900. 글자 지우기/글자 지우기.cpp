c874f47cfde7756a08e7a8b6f0aa79c6f6d84a86
Eun Joong Hwang <157799662+hej7866@users.noreply.github.com>
2025-10-30 20:07:49 +0900

    [level 0] Title: 글자 지우기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.cpp
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.cpp
@@ -7,14 +7,9 @@ using namespace std;
 
 string solution(string my_string, vector<int> indices) 
 {
-    sort(indices.begin(), indices.end()); // 오름차순 정렬
-    for(int i=0; i<indices.size(); i++)
-    {
-        my_string.erase(my_string.begin() + indices[i]);
-        for(int j = i + 1; j<indices.size(); j++)
-        {
-            indices[j]--;
-        }
+    sort(indices.begin(), indices.end(), greater<int>());
+    for (int i : indices) {
+        my_string.erase(i, 1);
     }
     return my_string;
 }
