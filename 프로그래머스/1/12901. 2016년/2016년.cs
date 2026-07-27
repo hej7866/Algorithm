4b94d6d2f2f5e220cdd8fb323133d996f119ed30
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-07-28 00:17:11 +0900

    [level 1] Title: 2016년, Time: 0.20 ms, Memory: 46.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cs
+++ 프로그래머스/1/12901. 2016년/2016년.cs
@@ -1,22 +1,21 @@
+using System.Collections.Generic;
+using System;
+
 public class Solution 
 {
     public string solution(int a, int b) 
     {
-        int[,] arr = new int[,]
-        {
-            {1, 31}, {2, 29}, {3, 31}, {4, 30},
-            {5, 31}, {6, 30}, {7, 31}, {8, 31},
-            {9, 30}, {10, 31}, {11, 30}, {12, 31},
-        };
+        int[] month = {0,31,29,31,30,31,30,31,31,30,31,30,31};
 
-        string[] v = new string[] {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        string[] week = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
 
-        int days = 0;
-        for(int i=0; i<a-1; i++)
+        int day = b;
+
+        for(int i = 1; i < a; i++)
         {
-            days += arr[i,1];
+            day += month[i];
         }
-        days += b - 1;
-        return v[days%7];
-    }
+
+        return week[day % 7];
+    } 
 }
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=csharp) 
 
 ### 성능 요약
 
-메모리: 3.64 MB, 시간: 0.04 ms
+메모리: 46.1 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 16일 23:51:06
+2026년 07월 28일 00:17:06
 
 ### 문제 설명
 
