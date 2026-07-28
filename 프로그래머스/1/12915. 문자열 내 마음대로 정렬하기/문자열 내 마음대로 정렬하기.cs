f517ef4c69c650e7899d0a5a670269bce2e62b8d
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-07-28 20:01:12 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 3.44 ms, Memory: 47.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+
+public class Solution
+{
+    public string[] solution(string[] strings, int n)
+    {
+        return strings
+            .OrderBy(x => x[n])
+            .ThenBy(x => x)
+            .ToArray();
+    }
+}
