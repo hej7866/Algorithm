478c59b1414d465c8ed04a17ef78a62fa8df6383
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-07-28 19:47:31 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.08 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
@@ -1,22 +1,12 @@
-public class Solution 
+using System;
+
+public class Solution
 {
-    public long solution(int a, int b) 
+    public long solution(int a, int b)
     {
-        long answer = 0;
-        if(a <= b)
-        {
-            for(int i=a; i<=b; i++)
-            {
-                answer += i;
-            }
-        }
-        else
-        {
-            for(int i=b; i<=a; i++)
-            {
-                answer += i;
-            }
-        }
-        return answer;
+        int min = Math.Min(a, b); // 작은값
+        int max = Math.Max(a, b); // 큰값
+
+        return ((long)min + max) * (max - min + 1) / 2;
     }
 }
