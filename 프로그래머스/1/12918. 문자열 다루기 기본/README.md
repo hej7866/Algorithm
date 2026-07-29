a4cbfb0ba375d1e28c6f7e93e74d50d52bfa1731
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-07-29 18:44:49 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.16 ms, Memory: 45.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/README.md
+++ 프로그래머스/1/12918. 문자열 다루기 기본/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 다루기 기본 - 12918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918) 
 
 ### 성능 요약
 
-메모리: 31.1 MB, 시간: 0.33 ms
+메모리: 45.6 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 15일 15:45:00
+2026년 07월 29일 18:44:43
 
 ### 문제 설명
 
--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cs
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cs
@@ -1,23 +1,20 @@
-using System;
-
 public class Solution 
 {
     public bool solution(string s) 
     {
-        bool isBool = true;
-        foreach(char c in s)
-        {
-            if(!Char.IsDigit(c))
-            {
-                isBool = false;
-                break;
-            }
-        }
-
         if(s.Length != 4 && s.Length != 6)
         {
-            isBool = false;
+            return false;
         }
-        return isBool;
+        
+        foreach(char c in s)
+        {
+            if(c >= 65)
+            {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
