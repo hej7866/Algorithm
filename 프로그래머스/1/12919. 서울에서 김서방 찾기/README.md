01117193a7350953a1fd4e873f02e6a48a473641
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-07-29 18:54:26 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.33 ms, Memory: 46.1 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 서울에서 김서방 찾기 - 12919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919) 
 
 ### 성능 요약
 
-메모리: 31 MB, 시간: 0.61 ms
+메모리: 46.1 MB, 시간: 0.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 15일 15:29:03
+2026년 07월 29일 18:54:22
 
 ### 문제 설명
 
--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -1,17 +1,18 @@
+using System;
+
 public class Solution 
 {
     public string solution(string[] seoul) 
     {
-        int idx = 0;
+        string ans = "";
         for(int i=0; i<seoul.Length; i++)
         {
-            if(seoul[i].Contains("Kim"))
+            if(seoul[i] == "Kim")
             {
-                idx = i;
+                ans = $"김서방은 {i}에 있다";
                 break;
             }
         }
-        string ans = "김서방은 " + idx.ToString() + "에 있다";
         return ans;
     }
 }
