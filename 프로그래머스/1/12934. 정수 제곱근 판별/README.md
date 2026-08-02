684d428fb619b55e89873f59ef3424d899a66e83
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-08-02 19:22:53 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 2.15 ms, Memory: 46.4 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/README.md
+++ 프로그래머스/1/12934. 정수 제곱근 판별/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 제곱근 판별 - 12934 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12934?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12934) 
 
 ### 성능 요약
 
-메모리: 31.3 MB, 시간: 0.18 ms
+메모리: 46.4 MB, 시간: 2.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 00:04:43
+2026년 08월 02일 19:22:50
 
 ### 문제 설명
 
--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
@@ -1,16 +1,20 @@
-using System;
-
 public class Solution 
 {
     public long solution(long n) 
     {
-        long root = (long)Math.Sqrt(n);
-        if (root * root == n)
+        long s = 1;
+        while(s * s < n)
         {
-            // n이 완전 제곱수일 경우 다음 완전 제곱수를 반환
-            return (root + 1) * (root + 1);
+            s++;
         }
-        // n이 완전 제곱수가 아닐 경우 -1 반환
-        return -1;
+    
+        if(s * s == n)
+        {
+            return (s+1) * (s+1);
+        }
+        else
+        {
+            return -1;
+        }
     }
 }
