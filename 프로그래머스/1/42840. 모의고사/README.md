a4f27559b946b07bd7871e0e4e08a6ffcc9c10b9
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-08-08 02:18:30 +0900

    [level 1] Title: 모의고사, Time: 3.00 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 31.3 MB, 시간: 5.04 ms
+메모리: 46.9 MB, 시간: 3.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 08일 05:28:05
+2026년 08월 08일 02:18:27
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.cs
+++ 프로그래머스/1/42840. 모의고사/모의고사.cs
@@ -1,57 +1,44 @@
 using System;
 using System.Collections.Generic;
-using System.Linq; 
+using System.Linq;
 
 public class Solution 
 {
     public int[] solution(int[] answers) 
     {
-        int[] arr1 = new int[5] {1,2,3,4,5};
-        int[] arr2 = new int[8] {2,1,2,3,2,4,2,5};
-        int[] arr3 = new int[10] {3,3,1,1,2,2,4,4,5,5};
+        int[] f = new int[] {1, 2, 3, 4, 5};
+        int[] s = new int[] {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] t = new int[] {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
-        
+        int[] scores = new int[3];
         for(int i=0; i<answers.Length; i++)
         {
-            if(answers[i] == arr1[i % arr1.Length]) { count1++; }
-            if(answers[i] == arr2[i % arr2.Length]) { count2++; } 
-            if(answers[i] == arr3[i % arr3.Length]) { count3++; }
-        }
-        
-        int[] countArr = new int[3] { count1, count2, count3 };
-        
-        var scores = new List<(int count, int student)> 
-        {
-            (count1, 1),
-            (count2, 2),
-            (count3, 3)
-        };
-
-        scores = scores.OrderByDescending(s => s.count)
-                       .ThenBy(s => s.student)
-                       .ToList();
-        
-        var result = new List<int>();
-        int highestScore = scores[0].count;
-
-        foreach (var score in scores) 
-        {
-            if (score.count == highestScore) 
+            if(f[i%5] == answers[i])
             {
-                result.Add(score.student);
+                scores[0]++;
+            }
+            if(s[i%8] == answers[i])
+            {
+                scores[1]++;
+            }
+            if(t[i%10] == answers[i])
+            {
+                scores[2]++;
             }
         }
-
+        
+        int max = scores.Max();
+        
+        List<int> result = new List<int>();
+        for(int i = 0; i < 3; i++)
+        {
+            if(scores[i] == max)
+            {
+                result.Add(i + 1);
+            }
+        }
+        
         return result.ToArray();
         
-        
-        
-        
-        
-        
-        return answers;
     }
 }
