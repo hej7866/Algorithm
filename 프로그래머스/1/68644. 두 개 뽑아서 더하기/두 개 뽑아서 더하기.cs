6729809889ee7927bea498dde83671a99b5fce9f
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-08-10 23:16:09 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.07 ms, Memory: 47.1 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cs
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cs
@@ -1,26 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Solution 
 {
     public int[] solution(int[] numbers) 
     {
-        HashSet<int> numSet = new HashSet<int>();
-        
+        HashSet<int> ans = new HashSet<int>();
         for(int i=0; i<numbers.Length - 1; i++)
         {
-            for(int j = i+1; j<numbers.Length; j++)
+            for(int j=i + 1; j<numbers.Length; j++)
             {
-                numSet.Add(numbers[i] + numbers[j]);
+                ans.Add(numbers[i] + numbers[j]);
             }
         }
-        
-        List<int> result = new List<int>();
-        foreach(int n in numSet)
-        {
-            result.Add(n);
-        }
-        result.Sort();
-        return result.ToArray();
+        return ans.OrderBy(x => x).ToArray();
     }
 }
