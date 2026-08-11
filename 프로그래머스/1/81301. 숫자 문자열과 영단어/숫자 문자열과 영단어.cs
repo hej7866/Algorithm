2a627d417b32b67ee7ff9d5bf2e592cd53fbe828
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-08-12 00:20:37 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.22 ms, Memory: 47.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
@@ -1,21 +1,24 @@
 using System;
+using System.Collections.Generic;
+
 
 public class Solution 
 {
     public int solution(string s) 
     {
-        string[] numArr = new string[10] 
+        Dictionary<string, string> numDict = new Dictionary<string, string>
         {
-            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+            {"zero", "0"}, {"one", "1"}, {"two", "2"}, {"three", "3"},
+            {"four", "4"}, {"five", "5"}, {"six", "6"}, {"seven", "7"},
+            {"eight", "8"}, {"nine", "9"}
         };
+
         
-        for(int i=0; i<=9; i++)
+        foreach (var pair in numDict)
         {
-            while(s.Contains(numArr[i]))
-            {
-                s = s.Replace(numArr[i], i.ToString());
-            }
+            s = s.Replace(pair.Key, pair.Value);
         }
         return Int32.Parse(s);
+        
     }
 }
