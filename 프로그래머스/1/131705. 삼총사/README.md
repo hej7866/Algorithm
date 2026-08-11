1220da29c594afce523c6ae83c55ff07866f971e
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-08-12 02:35:00 +0900

    [level 1] Title: 삼총사, Time: 0.14 ms, Memory: 46.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/README.md
+++ 프로그래머스/1/131705. 삼총사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 삼총사 - 131705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=csharp) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.01 ms
+메모리: 46.4 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 27일 02:24:47
+2026년 08월 12일 02:34:57
 
 ### 문제 설명
 
--- 프로그래머스/1/131705. 삼총사/삼총사.cs
+++ 프로그래머스/1/131705. 삼총사/삼총사.cs
@@ -1,35 +1,22 @@
 using System;
-using System.Collections.Generic;
 
 public class Solution 
 {
     public int solution(int[] number) 
     {
-        int idx = 0;
         int count = 0;
-        while(idx < number.Length-2)
+        for (int i = 0; i < number.Length; i++)
         {
-            for(int i=idx; i<number.Length-2; i++)
+            for (int j = i + 1; j < number.Length; j++)      // j는 i보다 큰 인덱스부터
             {
-                int[] numList = new int[3];
-                numList[0] = number[idx];
-                for(int j=i+1; j<number.Length-1; j++)
+                for (int k = j + 1; k < number.Length; k++)  // k는 j보다 큰 인덱스부터
                 {
-                    numList[1] = number[j];
-                    for(int k=j+1; k<number.Length; k++)
+                    if(number[i] + number[j] + number[k] == 0)
                     {
-                        numList[2] = number[k];
-                        int sum = 0;
-                        foreach(int num in numList)
-                        {
-                            sum += num;
-                        }
-                        if(sum == 0) { count++; }
+                        count++;
                     }
                 }
-                idx++;
             }
-            
         }
         return count;
     }
