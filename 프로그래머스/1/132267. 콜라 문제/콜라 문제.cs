0866a435a369e038226e6bab5e9267723742c253
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-08-12 23:17:03 +0900

    [level 1] Title: 콜라 문제, Time: 0.13 ms, Memory: 46.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -4,15 +4,15 @@ public class Solution
 {
     public int solution(int a, int b, int n) 
     {
-        int sum = 0;
-        int m = 0;
-        while(n>=a)
+        int count = 0;
+        while(n / a > 0)
         {
-            m = (n/a)*b;
-            n = n - ((n/a)*a) + (n/a)*b;
-            sum += m;
-            Console.WriteLine(m);
+            int t1 = n / a;
+            int t2 = t1 * a;
+            count += t1 * b;
+            n -= t2;
+            n += t1 * b;
         }
-        return sum;   
+        return count;
     }
 }
