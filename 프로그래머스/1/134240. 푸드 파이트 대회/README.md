bc8e3aa0cd4ec78504b18ccbfb080c5da6712eb6
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-08-13 01:06:21 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.51 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/README.md
+++ 프로그래머스/1/134240. 푸드 파이트 대회/README.md
@@ -1,10 +1,10 @@
 # [level 1] 푸드 파이트 대회 - 134240 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240) 
 
 ### 성능 요약
 
-메모리: 3.81 MB, 시간: 1.01 ms
+메모리: 46.7 MB, 시간: 0.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 25일 01:58:08
+2026년 08월 13일 01:06:18
 
 ### 문제 설명
 
--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
@@ -1,27 +1,25 @@
 using System;
+using System.Text;
 
 public class Solution 
 {
     public string solution(int[] food) 
     {
-        string str1 = "";
-        string str2 = "";
+        StringBuilder tmp = new StringBuilder();
         for(int i=1; i<food.Length; i++)
         {
-            int f = food[i] / 2;
-            for(int j=0; j<f; j++)
+            int count = food[i] / 2;
+            for(int j=0; j<count; j++)
             {
-                str1 += i.ToString();
+                tmp.Append(i.ToString());
             }
         }
-        for(int i=food.Length - 1; i>=1; i--)
-        {
-            int f = food[i] / 2;
-            for(int j=0; j<f; j++)
-            {
-                str2 += i.ToString();
-            }
-        }
-        return str1 + "0" + str2;
+        char[] chars = tmp.ToString().ToCharArray();
+        Array.Reverse(chars);
+        string reversed = new string(chars);
+        tmp.Append("0");
+        tmp.Append(reversed);
+        
+        return tmp.ToString();
     }
 }
