354185249716789d98cd0d690ac1b67784c1eefd
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-08-16 23:01:04 +0900

    [level 1] Title: 유연근무제, Time: 0.60 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.cs
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.cs
@@ -4,31 +4,29 @@ public class Solution
 {
     public int solution(int[] schedules, int[,] timelogs, int startday) 
     {
-        int n = timelogs.GetLength(0);
-        int result = n;
-        for(int i=0; i<n; i++) // n명을 순회하는데
+        int n = schedules.Length;
+        int count = n;
+        for(int i=0; i<n; i++)
         {
-            int schedule = schedules[i]/100 * 60 + schedules[i]%100; // 출근 희망시간을 정수로 환산
-            int currentDay = startday;
+            int start = startday;
             for(int j=0; j<7; j++)
             {
-                int timelog = timelogs[i,j]/100 * 60 + timelogs[i,j]%100; // 출근한 시간을 정수로 환산
-                Console.WriteLine($"{schedule}, {timelog}");
-                if(timelog - schedule > 10) // 일단 지각한 날인데
+                int tmp = start % 7;
+                if(ChangeFun(timelogs[i,j]) - ChangeFun(schedules[i]) > 10 && tmp != 6 && tmp != 0)
                 {
-                    
-                    if(currentDay%7 == 6 || currentDay%7 == 0) // 주말이면 패스하고
-                    {
-                        currentDay = currentDay == 7 ? 1 : currentDay + 1;
-                        continue;
-                    }
-                    result--; // 평일인데 10보다 차이가 크면 얘 제외 
-                    break; //다음 사람으로 넘어가
+                    count--;
+                    break;
                 }
-                currentDay = currentDay == 7 ? 1 : currentDay + 1;
+                start += 1;
             }
-            
         }
-        return result;
+        return count;
+    }
+    
+    int ChangeFun(int time)
+    {
+        int h = time / 100;
+        int m = time % 100;
+        return h * 60 + m;
     }
 }
