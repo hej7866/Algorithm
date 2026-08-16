1afd6f186aa3fee5c6302f24774ac8b408ab3685
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-08-16 22:29:42 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.07 ms, Memory: 47.7 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCP 기출문제] 1번 / 동영상 재생기 - 340213 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213) 
 
 ### 성능 요약
 
-메모리: 31.6 MB, 시간: 4.58 ms
+메모리: 47.7 MB, 시간: 1.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 29일 19:27:36
+2026년 08월 16일 22:29:35
 
 ### 문제 설명
 
--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cs
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cs
@@ -2,83 +2,52 @@ using System;
 
 public class Solution 
 {
-    public string solution(string video_len, string pos, string op_start, string op_end, string[] commands) 
+    public string solution(string video_len, string pos, string op_start, string op_end, string[] commands)     
     {
-        string[] vArr = video_len.Split(':');
-        string[] pArr = pos.Split(':');
-        string[] sArr = op_start.Split(':');
-        string[] eArr = op_end.Split(':');
+        // 정수화
+        int video_len_num = ChangeNumber(video_len);
+        int pos_num = ChangeNumber(pos);
+        int op_start_num = ChangeNumber(op_start);
+        int op_end_num = ChangeNumber(op_end);
         
-        int v = Int32.Parse(vArr[0]) * 60 + Int32.Parse(vArr[1]);
-        int p = Int32.Parse(pArr[0]) * 60 + Int32.Parse(pArr[1]);
-        int s = Int32.Parse(sArr[0]) * 60 + Int32.Parse(sArr[1]);
-        int e = Int32.Parse(eArr[0]) * 60 + Int32.Parse(eArr[1]);
+        if (op_start_num <= pos_num && pos_num <= op_end_num)
+        {
+            pos_num = op_end_num;
+        }
+        
+        foreach(string command in commands)
+        {
+            if (command == "next")
+            {
+                pos_num += 10;
+                if (pos_num > video_len_num) pos_num = video_len_num;
+            }
+            else // prev
+            {
+                pos_num -= 10;
+                if (pos_num < 0) pos_num = 0;
+            }
             
-        for(int i=0; i<commands.Length; i++)
-        {
-            if(s <= p && p <= e)
+            // 2. 매 명령 실행 후 무조건 체크 (양쪽 <= 로 통일)
+            if (op_start_num <= pos_num && pos_num <= op_end_num)
             {
-                p = e;
-                if(commands[i] == "next")
-                {
-                    p += 10; 
-                    if(p > v) { p = v; }
-                }
-                else if(commands[i] == "prev")
-                {
-                    p -= 10;
-                    if(p < 0) { p = 0; }
-                }
-            }
-            else
-            {
-                if(commands[i] == "next")
-                {
-                    p += 10;
-                    if(p > v) { p = v; }
-                }
-                else if(commands[i] == "prev")
-                {
-                    p -= 10;
-                    if(p < 0) { p = 0; }
-                }
+                pos_num = op_end_num;
             }
         }
-        
-        if(s <= p && p <= e)
-        {
-            p = e;
-        }
-        
-        Console.Write("{0} {1} {2} {3}",v, p, s, e);
         
         string result = "";
-        
-        if(p/60 != 0)
-        {
-            if(p/60 < 10)
-            {
-                result += "0" + (p/60).ToString();
-            }
-            else
-            {
-                result += (p/60).ToString();
-            }
-        }
-        else { result += "00"; }
+        result += (pos_num / 60 < 10 ? "0" : "") + (pos_num / 60).ToString();
         result += ":";
-        if(p%60 != 0)
-        {
-            if(p%60 < 10)
-            {
-                result += "0" + (p%60).ToString();
-            }
-            else
-            {
-                result += (p%60).ToString();
-            }
-        }
-        else { result += "00"; }
+        result += (pos_num % 60 < 10 ? "0" : "") + (pos_num % 60).ToString();
+        
         return result;
+    }
+    
+    int ChangeNumber(string time)
+    {
+        string[] timeSplit = time.Split(':');
+        int m = Int32.Parse(timeSplit[0]) * 60;
+        int s = Int32.Parse(timeSplit[1]);
+        return m + s;
     }
 }
