c24a0cd563f3b9c32f2acbe0b6cf0e603c1b31c6
Eun Joong Hwang <157799662+DipleGames@users.noreply.github.com>
2026-08-17 03:17:01 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.31 ms, Memory: 46.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cs
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cs
@@ -1,32 +1,43 @@
-public class Solution
+using System;
+using System.Collections.Generic;
+
+class Solution 
 {
-    public int solution(int n)
+    public int solution(int n) 
     {
-        int targetCount = CountOne(n);
-        int answer = n + 1;
-
-        while (CountOne(answer) != targetCount)
-        {
-            answer++;
-        }
-
-        return answer;
-    }
-
-    private int CountOne(int number)
-    {
+        Stack<int> s = new Stack<int>();
         int count = 0;
-
-        while (number > 0)
+        int num = n;
+        while(num > 0)
         {
-            if (number % 2 == 1)
+            int remainder = num % 2;   // 나머지 (0 또는 1)
+            if(remainder == 1)
             {
                 count++;
             }
-
-            number /= 2;
+            num /= 2;   
         }
 
-        return count;
+        int count2 = 0;
+        int i = 1;
+        int num2 = n + i;
+        while(count2 != count)
+        {
+            count2 = 0;
+            while(num2 > 0)
+            {
+                int remainder = num2 % 2;   // 나머지 (0 또는 1)
+                if(remainder == 1)
+                {
+                    count2++;
+                }
+                num2 /= 2;   
+            }
+            i++;
+            num2 = n + i; 
+        }
+        
+       
+        return n + i - 1;
     }
 }
